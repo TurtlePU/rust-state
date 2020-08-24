82f9ffa8cb606285f3c57f82905a3767f30f8983
TurtlePU <sokolov.p64@gmail.com>
2020-08-25 01:20:24 +0300

    + Transit

--- src/article/state.rs
+++ src/article/state.rs
@@ -1,3 +1,20 @@
+use crate::article::Article;
+
 pub trait State {
-    // empty
+    fn send_to_moderators(&mut self) -> Transit {
+        Transit(None)
+    }
+}
+
+pub struct Transit(pub Option<Box<dyn State>>);
+
+impl Transit {
+    pub fn to(state: impl State + 'static) -> Self {
+        Self(Some(Box::new(state)))
+    }
+
+    pub fn apply(self, article: &mut Article) -> Option<()> {
+        article.state = self.0?;
+        Some(())
+    }
 }
